d6ea4c5675fab837cb623a9338239c6722c99a14
AtifxUMTS <atifalishahnaqvi3345@gmail.com>
2023-09-23 10:59:11 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let personName = "Atif Ali Shah";
+// step 1 show in lower case:
+let lowercaseName = personName.toLowerCase();
+console.log(lowercaseName);
---
+++ index.ts
@@ -0,0 +1,5 @@
+let personName: string = "Atif Ali Shah";
+
+// step 1 show in lower case:
+let lowercaseName: string = personName.toLowerCase();
+console.log(lowercaseName);
